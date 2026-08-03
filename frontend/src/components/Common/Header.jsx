d8c085bf2df232559140ef192134e175256a5df2
Akshay <mandaakshay20@gmail.com>
2026-08-03 15:52:55 +0530

    Completed search bar

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar';
 
 
 const Header = () => {
   return (
-    <div>
+    <header className="border-b border-gray-200">
       {/* Topbar component */}
         <Topbar />
-    </div>
-  )
-}
+      {/* Navbar component */}
+        <Navbar />
+    </header>
+  );
+};
 
 export default Header
