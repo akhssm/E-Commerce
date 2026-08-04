418902d846317561679c8cd04e4722e21a444e41
Akshay <mandaakshay20@gmail.com>
2026-08-04 11:48:25 +0530

    Completed Footer section

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer';
 
 const UserLayout = () => {
   return (
     <>
       {/* Header component */}
       <Header />
+      {/* Main Content */}
+      
+      {/* Footer component */}
+      <Footer />
     </>
   )
 };
