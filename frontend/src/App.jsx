ddd48e8d605fce8d1ad4e94ff2ac65ac4fa532bb
Akshay <mandaakshay20@gmail.com>
2026-08-05 16:35:26 +0530

    Changed sonner from root folder to frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Toaster position="top-right" />
       {/* Your routes and components go here */}
       <Routes>
        <Route path="/" element={<UserLayout />}>
--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { toast } from "sonner";
 
 const selectedProduct = {
     name: "Stylish Jacket",
@@ -38,6 +39,12 @@ const ProductDetails = () => {
         if (action === "plus") setQuantity((prev) => prev + 1);
         if (action === "minus" && quantity > 1) setQuantity((prev) => prev - 1);
     };
+
+    const handleAddToCart = () => {
+        if (!selectedSize || !selectedColor) {
+            toast.error("Please select a size and color before adding to cart")
+        }
+    }
 
   return (
     <div className="p-6">
@@ -152,7 +159,10 @@ const ProductDetails = () => {
                         </div>
                    </div>
 
-                   <button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
+                   <button
+                     onClick={handleAddToCart}
+                     className="bg-black text-white py-2 px-6 rounded w-full mb-4"
+                    >
                       ADD TO CART
                     </button>
 
