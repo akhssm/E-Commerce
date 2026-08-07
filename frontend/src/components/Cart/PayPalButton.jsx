5f1b26cf209e1be83f4e6c8b98aab60c1117d09d
Akshay <mandaakshay20@gmail.com>
2026-08-07 17:25:22 +0530

    Started paypal Integration

---
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+const PayPalButton = ({ amount, onSuccess, onError }) => {
+  return (
+    <PayPalScriptProvider
+      options={{
+        "client-id":
+           "BAAcCiymFcL_YCmVqg4cChpVO-y0K8tvKNvQrheQ1OoAuURAGWTRJOon3saq9n7lQLVKz1Vd6yOVR-qZtU",
+      }}
+    >
+        <PayPalButton
+          style={{ layout: "vertical" }}
+          createOrder={(data, actions) => {
+            return actions.order.create({
+                purchase_units: [{ amount: { value: amount } }],
+            });
+          }}
+          onApprove={(data, actions) => {
+            return actions.order.capture().then(onSuccess);
+          }}
+          onError={onError}
+        />
+    </PayPalScriptProvider>
+  );
+};
+
+export default PayPalButton;
