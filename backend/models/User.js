93116b4d82ccdac4167d74c0bd570aabeafd8ea2
Akshay <mandaakshay20@gmail.com>
2026-08-23 11:27:13 +0530

    Made Small Change

--- backend/models/User.js
+++ backend/models/User.js
@@ -27,20 +27,11 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true});
 
 // Password Hash middleware - WORKS WITHOUT CRASHING
-userSchema.pre("save", function(next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
-  
-  const user = this;
-  
-  bcrypt.genSalt(10)
-    .then(salt => bcrypt.hash(user.password, salt))
-    .then(hash => {
-      user.password = hash;
-      next();
-    })
-    .catch(err => next(err));
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Match User entered password to Hashed password
