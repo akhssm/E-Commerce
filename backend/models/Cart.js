e61e567f67401bd8537e1a18e7c2ecdab5ccf981
Akshay <mandaakshay20@gmail.com>
2026-08-28 18:25:47 +0530

    Started CartRoutes

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -33,4 +33,4 @@ const cartSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-module.exports = mongoose.model("Cart", cartItemsSchema);
+module.exports = mongoose.model("Cart", cartSchema);
